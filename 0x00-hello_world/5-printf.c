ce996d8f42ade8f781eb792f5c183ad61af6b22c
Uzo Pius Enyum <uzopiusenyum@gmail.com>
2023-02-16 07:29:34 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - A program that print a line usine the print function
- * Return: 0 (sucess)
+ * main - A program that print a line using the printf function
+ * Return: 0 (Success)
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a pience of art,\n")
-	return (0);
+printf("with proper grammar, but the outcome is a pience of art,\n")
+return (0);
 }
