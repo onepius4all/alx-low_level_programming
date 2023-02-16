3fd9f8e9da3bcc181cf9d79391126133fffb078f
Uzo Pius Enyum <uzopiusenyum@gmail.com>
2023-02-16 07:05:42 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that print a line usine the print function
+ * Return: 0 (sucess)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a pience of art,\n")
+	return (0);
