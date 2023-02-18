064081553ebcdedc6d3d53dad4bc24ed4cb30631
Uzo Pius Enyum <uzopiusenyum@gmail.com>
2023-02-17 18:00:32 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
 * main - Entry point
-*
+* Description: prints the alphabet in lowercase\n
 * Return: Always 0 (success)
 */
 int main(void)
@@ -14,6 +15,6 @@ while
 	putchar(c);
 	c++;
 	}
-putchar\n');
+putchar('\n');
 Return(0);
 }
