4a672b7fbb2c6ea3c71ea9779590da4386978e6b
Uzo Pius Enyum <uzopiusenyum@gmail.com>
2023-02-22 13:54:26 -0800

    10-add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,12 +1,16 @@
 #include "main.h"
-/**
- * add - adds two numbers
- * @a: para 1
- * @b: para 2
- * Return: sum
- */
-{
 
+/**
+ * add - adds two integers and returns the result
+ * @a: int to be added to b
+ * @b: int to be added to a
+ * Return: sum of a and b
+ */
 int add(int a, int b)
-	return (a + b);
+
+{
+	int c;
+
+	c = a + b;
+return (c);
 }
