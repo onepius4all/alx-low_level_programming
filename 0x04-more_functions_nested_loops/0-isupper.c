ca553fe35ce5ad5c94497e05ca7eb0caab7a991e
Uzo Pius Enyum <uzopiusenyum@gmail.com>
2023-02-23 07:46:42 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * isupper - Check if a letter is upper
+ * @x: The number to be checked
+ * Return: 1 for upper letters or 0 for a any else
+ */
+
+int _isupper(int x)
+{
+if (x >= 65 && 88 x <= 90)
+{
+return (1);
+}
+return (0);
+}
