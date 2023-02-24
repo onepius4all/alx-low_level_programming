a70f4788a24b720b37ae40f1d22e8e67d4fe476d
Uzo Pius Enyum <uzopiusenyum@gmail.com>
2023-02-23 20:25:21 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,19 @@
 #include "main.h"
-
 /**
- * print_numbers - print the number since 0 up to 9
- * Return: The numbers since 0 up to 9
+ * print_numbers - checks for checks for a digit (0 through 9).
+ *
+ * Return: Always 0.
  */
-void print_numbers(void)
-{
-	int x;
 
-	for (x = 0; x <= 9; x++)
-	{
-	_putchar(x + '0');
-	}
-	_putchar('\n');
+void print_numbers(void)
+
+{
+
+int c;
+
+for (c = 48; c < 58; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
 }
