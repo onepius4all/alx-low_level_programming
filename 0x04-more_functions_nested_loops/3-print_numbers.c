d675905af42f1196b0c4fe8d9befed1fb3ba3b48
Uzo Pius Enyum <uzopiusenyum@gmail.com>
2023-02-23 17:12:10 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,8 @@
-#include "main"
+#include "main.h"
 
 /**
  * print_numbers - print the number since 0 up to 9
- * Return: The number 0 up to 9
+ * Return: The numbers since 0 up to 9
  */
 void print_numbers(void)
 {
