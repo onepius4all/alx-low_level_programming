ebc02a8a543ab8f5a51447e29a8c95c66f2021de
Uzo Pius Enyum <uzopiusenyum@gmail.com>
2023-03-20 15:54:55 -0700

    2-main.c

--- 0x0D-preprocessor/main.h
+++ 0x0D-preprocessor/main.h
@@ -1,7 +1,11 @@
-#ifndef OBJECT_LIKE_MACRO_H
-#define OBJECT_LIKE_MACRO_H
+#ifndef MAIN_H
+#define MAIN_H
 
-#define SIZE 1024
+#include <stdio.h>
+#include "0-object_like_macro.h"
+#include "1-pi.h"
+#include "3-function_like_macro.h"
+#include "4-sum.h"
 
 #endif
 
