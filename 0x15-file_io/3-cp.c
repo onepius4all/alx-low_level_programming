af8ac62eca53f918c220f85e65faa76561d9c9b3
Uzo Pius Enyum <uzopiusenyum@gmail.com>
2023-04-10 16:54:26 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -30,13 +30,13 @@ return (buffer);
 * @f: The file descriptor to be closed.
 */
 
-void close_file(int f)
+void close_file(int fd)
 {
 int c;
-c = close(f);
+c = close(fd);
 if (c == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't close f %d\n", f);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 exit(100);
 }
 
